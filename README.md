b87a3c80f84f29f6323fa84a90a2e05db30f3056
Smerth <merth@me.com>
2018-03-05 19:00:29 -0800

    Basic Animation with CSS keyframes

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Tutorial project from [Lynda.com course](https://www.lynda.com/React-js-tutorial
 * Release - 1.3.0 Use Composes to leverage other styles
 * Release - 1.4.0 Set up state and grid component
 * Release - 1.5.0 Complete GridItem
+* Release - 1.6.0 Basic Animation with CSS keyframes
--- src/Grid/GridItem.js
+++ src/Grid/GridItem.js
@@ -1,14 +1,13 @@
 import React from "react";
-
-//styles
 import styles from "./GridItem.module.css";
 
-export default class GridItem extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>GridItem</h1>
-      </div>
-    );
-  }
-}
+const GridItem = props => {
+  return (
+    <div className={styles.card}>
+      <h2 className={styles.title}>{props.fact.title}</h2>
+      <article className={styles.fact}>{props.fact.fact}</article>
+    </div>
+  );
+};
+
+export default GridItem;
